06396a2655602eb5ad2554929e2de2293d91c567
bokuweb <bokuweb12@gmail.com>
2022-10-26 03:07:45 +0000

    Update rc to output generated at b471111

--- src/report.ts
+++
@@ -1,5 +0,0 @@
-const REPORT_PAGE = 'https://reg-actions.pages.dev/';
-
-export const createReportURL = (owner: string, reponame: string, runId: number) => {
-  return `${REPORT_PAGE}?owner=${owner}&repository=${reponame}&run_id=${runId}`;
-};
