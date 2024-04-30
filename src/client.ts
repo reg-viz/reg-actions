d1b7dccbc4b9e4d89485a7161eaa9a7a68e16353
bokuweb <bokuweb12@gmail.com>
2024-04-30 13:44:48 +0900

    fix

--- src/client.ts
+++ src/client.ts
@@ -40,7 +40,7 @@ export const createClient = (repository: Repository, octokit: Octokit) => {
       const { downloadPath } = await backOff(
         () =>
           artifactClient.downloadArtifact(artifactId, {
-            path: join(workspace(), '__reg_download/reg'),
+            path: join(workspace(), '__reg_download/reg.zip'),
             findBy: {
               token,
               workflowRunId: runId,
